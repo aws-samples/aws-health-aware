dcd7fae27c3ccdbcc7a623fe06b75a6ef154118f
Lance A. Brown <lance.a.brown@nasa.gov>
2023-05-09 15:42:39 -0500

    Remove deprecated template_file call
    The template provider is deprecated by Hashicorp. Remove
    the data resource calling the template function and replace
    it with direct calls to the file call in the lambda_zip resource.

--- terraform/Terraform_DEPLOY_AHA/Terraform_DEPLOY_AHA.tf
+++ terraform/Terraform_DEPLOY_AHA/Terraform_DEPLOY_AHA.tf
@@ -29,20 +29,16 @@ provider "aws" {
 locals {
     source_files = ["../../handler.py", "../../messagegenerator.py"]
 }
-data "template_file" "t_file" {
-    count = "${length(local.source_files)}"
-    template = "${file(element(local.source_files, count.index))}"
-}
 data "archive_file" "lambda_zip" {
     type          = "zip"
     output_path   = "lambda_function.zip"
     source {
       filename = "${basename(local.source_files[0])}"
-      content  = "${data.template_file.t_file.0.rendered}"
+      content  = file("${local.source_files[0]}")
     }
     source {
       filename = "${basename(local.source_files[1])}"
-      content  = "${data.template_file.t_file.1.rendered}"
+      content  = file("${local.source_files[1]}")
   }
 }
 
